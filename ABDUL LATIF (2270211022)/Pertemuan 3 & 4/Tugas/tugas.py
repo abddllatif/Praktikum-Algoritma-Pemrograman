c496cb1137e0fb5d9814431bd1931730b6c0a787
abddllatif <118619131+abddllatif@users.noreply.github.com>
2022-11-25 22:07:18 +0700

    Add files via upload
    Push

---
+++ ABDUL LATIF (2270211022)/Pertemuan 3 & 4/Tugas/tugas.py
@@ -0,0 +1,27 @@
+print("======================================================")
+print("INVOICE LATIF BARBER")
+print("Bulak tinggi, Bekasi")
+import datetime
+x=datetime.datetime.now()
+print(x)
+print("======================================================")
+str(input("Nama Pelanggan : "))
+
+str(input("Alamat : "))
+
+str(input("Nomor Telepon : "))
+
+import datetime
+x=datetime.datetime.now()
+print(x)
+
+print("Potong Rambut + Keramas")
+print("Total : Rp 25.000")
+print("======================================================")
+Tagihan= int(input("Tagihan: "))
+Bayar= int(input("Uang yang dibayar: "))
+Kembalian =Bayar-Tagihan
+print("___")
+print("Kembalian ", Kembalian)
+
+print("Terimakasih sudah datang di barber kami ")
